96c4e7bc2c58a0891097e7ceb13fabbc1898791e
Crowdbotics <team@crowdbotics.com>
2021-11-27 06:31:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ukelele_32177",
+  url: "https://ukelele_32177.botics.co",
+  api: "https://ukelele_32177.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
